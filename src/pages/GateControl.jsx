97b61062889cef3ee24fc37fd62b0a2d33767e50
mdepraba <prabakusuma764@gmail.com>
2025-05-20 01:36:03 +0800

    fix(style): remove container className

--- src/pages/GateControl.jsx
+++ src/pages/GateControl.jsx
@@ -2,7 +2,7 @@ import StatusAlert from "../components/StatusAlert";
 
 function GateControl() {
   return (
-    <div className="container p-6">
+    <div className="p-6">
       <StatusAlert />
       <h1>Gate Control</h1>
       <p>Control the gate from here.</p>
